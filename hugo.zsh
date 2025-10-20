3fd38cbb3a6ca8408506c75f14e18a99530a0c9f
NicHub <nicolas.jeanmonod@gmail.com>
2025-10-20 08:22:22 +0200

    https for local hugo server

--- hugo.zsh
+++ hugo.zsh
@@ -3,19 +3,26 @@ rm -rf public
 source $HOME/.zshrc
 ip >/dev/null 2>&1
 
+MINIFY=false
+[[ "$MINIFY" != "true" ]] && unset MINIFY
+
+USE_HTTPS=true
+[[ "$USE_HTTPS" != "true" ]] && unset USE_HTTPS
+PROTOCOL=http${USE_HTTPS:+s}
+
 PORT="1313"
-BASE_URL="http://$IP"
+BASE_URL="$PROTOCOL://$IP"
 FULL_URL="$BASE_URL:$PORT"
 qrencode -t ANSI $FULL_URL
 echo -e "\n\n$FULL_URL\n\n"
 
-hugo server             \
-    -D                  \
-    --gc                \
-    --disableFastRender \
-    --baseURL=$BASE_URL \
-    --bind=$IP          \
-    --port=$PORT        \
+hugo server                 \
+    -D                      \
+    --gc                    \
+    --disableFastRender     \
+    --baseURL=$BASE_URL     \
+    --bind=$IP              \
+    --port=$PORT            \
+    ${USE_HTTPS:+--tlsAuto} \
+    ${MINIFY:+--minify}     \
     --appendPort=true
-
-    # --minify            \
