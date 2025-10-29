adfaecea7035e65c4d96554e4fa173eb17b9c61b
NicHub <nicolas.jeanmonod@gmail.com>
2025-10-29 10:57:12 +0100

    no minify

--- hugo.zsh
+++ hugo.zsh
@@ -9,16 +9,13 @@ rm -rf public
 source $HOME/.zshrc
 ip >/dev/null 2>&1
 
-MINIFY=false
-[[ "$MINIFY" != "true" ]] && unset MINIFY
-
 USE_HTTPS=$([[ "$1" == "https" ]] && echo true || unset USE_HTTPS)
 PROTOCOL=http${USE_HTTPS:+s}
 
 PORT="1313"
 BASE_URL="$PROTOCOL://$IP"
 FULL_URL="$BASE_URL:$PORT"
-qrencode -t ANSI $FULL_URL
+qrencode -t UTF8 $FULL_URL
 echo -e "\n\n$FULL_URL\n\n"
 
 hugo server                 \
@@ -29,5 +26,4 @@ hugo server                 \
     --bind=$IP              \
     --port=$PORT            \
     ${USE_HTTPS:+--tlsAuto} \
-    ${MINIFY:+--minify}     \
     --appendPort=true
